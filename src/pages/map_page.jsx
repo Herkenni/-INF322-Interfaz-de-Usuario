2f9cd524a8aa6d5d2599822e1036f9418d4a4cbe
Vicenzo Brante <vicenzo.brante@usm.cl>
2024-11-02 23:09:34 -0300

    Mapa con iconos e info

---
+++ src/pages/map_page.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+import Papel from '../assets/Papel.png'
+import Mapa from '../assets/Mapa.png'
+import Plastico from '../assets/Plastico.png'
+import Vidrio from '../assets/Vidrio.png'
+
+export const MapPage = () => {
+    
+  
+    return (
+        <div>
+            
+        </div>
+    )
+  }
+  
+  export default MapPage
